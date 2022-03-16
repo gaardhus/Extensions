44c1c60fe39fe3dbda4a2ee51280014bcba1293f
Tobias Gårdhus <tobias_gaardhus@hotmail.com>
2022-03-16 19:38:15 +0100

    Changed hotkeys and formatted JS

--- overleaf_focus/fullscreen.js
+++ overleaf_focus/fullscreen.js
@@ -1,10 +1,3 @@
-// $(".ace-editor-wrapper").resizable({
-//     handles: 'n,w,s,e',
-//     minWidth: 200,
-//     maxWidth: 400,
-// });
-
-
 /*
 
 Styling
@@ -12,35 +5,35 @@ Styling
 */
 
 function createCSS() {
-  element = document.getElementById("customCSS")
-  if (element) {
-    console.log("Removing old CSS")
-    element.parentNode.removeChild(element)
-  }
+	element = document.getElementById("customCSS")
+	if (element) {
+		console.log("Removing old CSS")
+		element.parentNode.removeChild(element)
+	}
 
-  let style = document.createElement('style');
-  style.id = "customCSS"
-  document.getElementsByTagName('head')[0].appendChild(style)
-  return style
+	let style = document.createElement('style');
+	style.id = "customCSS"
+	document.getElementsByTagName('head')[0].appendChild(style)
+	return style
 }
 
 function updateStyle() {
-  console.log('updating style')
+	console.log('updating style')
 
-  if (document.getElementsByName("editorTheme")[0].value === "textmate") {
+	if (document.getElementsByName("editorTheme")[0].value === "textmate") {
 
-  }
-  setTimeout(() => { 
-    editorBody = document.querySelector(".ace_editor")
+	}
+	setTimeout(() => {
+		editorBody = document.querySelector(".ace_editor")
 
-    styleColor = window.getComputedStyle(editorBody, null).getPropertyValue("background-color")
-    
-    console.log(styleColor)
-  
-    style = createCSS()
+		styleColor = window.getComputedStyle(editorBody, null).getPropertyValue("background-color")
 
-    // add the CSS as a string
-    style.textContent = `
+		console.log(styleColor)
+
+		style = createCSS()
+
+		// add the CSS as a string
+		style.textContent = `
     #editor {
       background: ${styleColor} !important
     }
@@ -70,17 +63,8 @@ function updateStyle() {
       background: ${styleColor} !important
     }
     `;
-  }, 200);
+	}, 200);
 }
-
-// Add eventlistener to theme changer
-var editorTheme = document.getElementsByName("editorTheme")[0]
-
-editorTheme.addEventListener("change", updateStyle, true)
-
-// Get current style
-updateStyle()
-
 
 /*
 
@@ -91,69 +75,79 @@ Fullscreen
 // Get OS function
 
 function getOS() {
-    var userAgent = window.navigator.userAgent,
-        platform = window.navigator.platform,
-        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-        os = null;
-  
-    if (macosPlatforms.indexOf(platform) !== -1) {
-      os = 'Mac';
-    } else if (iosPlatforms.indexOf(platform) !== -1) {
-      os = 'iOS';
-    } else if (windowsPlatforms.indexOf(platform) !== -1) {
-      os = 'Windows';
-    } else if (/Android/.test(userAgent)) {
-      os = 'Android';
-    } else if (!os && /Linux/.test(platform)) {
-      os = 'Linux';
-    }
-  
-    return os;
-  }
-  
+	var userAgent = window.navigator.userAgent,
+		platform = window.navigator.platform,
+		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+		os = null;
+
+	if (macosPlatforms.indexOf(platform) !== -1) {
+		os = 'Mac';
+	} else if (iosPlatforms.indexOf(platform) !== -1) {
+		os = 'iOS';
+	} else if (windowsPlatforms.indexOf(platform) !== -1) {
+		os = 'Windows';
+	} else if (/Android/.test(userAgent)) {
+		os = 'Android';
+	} else if (!os && /Linux/.test(platform)) {
+		os = 'Linux';
+	}
+
+	return os;
+}
+
+
+function requestFullscreen(element) {
+	if (element.requestFullscreen) {
+		element.requestFullscreen()
+	} else if (element.webkitRequestFullscreen) {
+		element.webkitRequestFullscreen()
+	}
+}
+
+function cancelFullscreen() {
+	if (document.exitFullscreen) {
+		document.exitFullscreen()
+	} else if (document.webkitCancelFullscreen) {
+		document.webkitCancelFullscreen()
+	}
+}
+
 // define fullscreen handler
 function fullscreen(e) {
 
-    var OS = getOS()
+	var OS = getOS()
 
-    if (OS == "Mac") {
-        var platform_key = e.metaKey
-        
-        function requestFullscreen(element) {
-          element.webkitRequestFullscreen()
-        }
-        
-        function cancelFullscreen() {
-          document.webkitCancelFullscreen()
-        }
-        
-    } else {
-        var platform_key = e.ctrlKey
-        
-        function requestFullscreen(element) {
-          element.requestFullscreen()
-        }
+	if (OS == "Mac") {
+		var platform_key = e.metaKey && e.ctrlKey
 
-        function cancelFullscreen() {
-          document.exitFullscreen()
-        }
-    }
+	} else {
+		var platform_key = e.ctrlKey
 
-    // Enter fullscreen
-    if (platform_key && e.key === 'ArrowUp') {
-        // call your function to do the thing
-        editor = document.querySelector("#editor")
-        requestFullscreen(editor)
-        
-    }
-    
-    // Exit fullscreen
-    else if (platform_key && e.key === 'ArrowDown') {
-        cancelFullscreen()
-    }
+		// Enter fullscreen
+		if (platform_key && e.key === 'ArrowUp') {
+			// call your function to do the thing
+			editor = document.querySelector("#editor")
+			requestFullscreen(editor)
+		}
+
+		// Exit fullscreen
+		else if (platform_key && e.key === 'ArrowDown') {
+			cancelFullscreen()
+		}
+	}
 }
 
-// register the handler 
-document.addEventListener('keyup', fullscreen, false);
+// register the handlers
+
+// Add eventlistener to theme changer
+var editorTheme = document.getElementsByName("editorTheme")[0]
+
+editorTheme.addEventListener("change", updateStyle, true)
+
+// Get current style
+updateStyle()
+
+// Add eventlistener for fullscreen
+document.addEventListener('keyup', fullscreen, false)
